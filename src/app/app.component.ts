0c4519d25d43741403c6ef6fc7d516fb5b7cfa7f
Eloísa Alés Esquivel <eloisa.alesqui@gmail.com>
2018-07-11 21:19:04 +0200

    Modificados icon y splash screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,12 @@ import { TecladoPage } from './pages/teclado/teclado.component';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage: any = TecladoPage;
+  rootPage:any = TecladoPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
   }
 }
-
